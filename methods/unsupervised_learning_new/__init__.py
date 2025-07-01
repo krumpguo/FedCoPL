db83417362fa82c8da84e6fee1f9a772c5518083
郭况浦 <gkp@mail.ustc.edu.cn>
2025-07-01 09:45:00 +0800

    Update __init__.py

--- methods/unsupervised_learning_new/__init__.py
+++ methods/unsupervised_learning_new/__init__.py
@@ -1,4 +1,4 @@
 from .training_strategies import TrainingStrategy
 from .prompt import TextualPrompt
-from .our_dual_local_prompt import Our_Dual_Local_Prompt
+from .fedcopl import FedCoPL
 from .textual_PL import TextualFPL_PL
